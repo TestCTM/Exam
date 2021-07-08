405b2d9588235c7dcc8d05e5acbaa10e21cfa111
LuisMartinez <51487802+WichoMartinez@users.noreply.github.com>
2021-07-08 17:51:29 -0500

    Code Challenge Fizz Buzz

---
+++ ApiExam/ApiExam/Controllers/MainController.cs
@@ -0,0 +1,28 @@
+﻿using ApiExam.Logic;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ApiExam.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MainController : ControllerBase
+    {
+        // GET: api/<MainController>
+        [HttpGet("{n}")]
+        public IEnumerable<string> Get(int n)
+        {
+            Operations operations = new Operations();
+            return operations.FizzBuzz(n);
+        }
+
+       
+
+        
+    }
+}
---
+++ ApiExam/ApiExam/Logic/Operations.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiExam.Logic
+{
+    public class Operations
+    {
+        /// <summary>
+        /// For each multiple of 3, print "Fizz" instead of the number.
+        /// For each multiple of 5, print "Buzz" instead of the number.
+        /// For numbers which are multiples of both 3 and 5, print "FizzBuzz" instead of the number
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public List<string> FizzBuzz(int n) {
+
+            List<string> listFizzBuzz = new List<string>();
+
+
+            for (int i = 1; i <= n; i++)
+            {
+
+                if (i % 3 == 0 && i % 5 != 0)
+                    listFizzBuzz.Add("Fizz");
+                else if (i % 5 == 0 && i % 3 != 0)
+                    listFizzBuzz.Add("Buzz");
+                else if (i % 5 == 0 && i % 3 == 0)
+                    listFizzBuzz.Add("FizzBuzz");
+                else
+                    listFizzBuzz.Add(i.ToString());
+            }
+
+
+
+            return listFizzBuzz;
+
+        }
+
+    }
+}
